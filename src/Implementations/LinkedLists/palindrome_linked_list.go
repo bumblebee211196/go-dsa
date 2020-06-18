f2d55f90fd42150faf2f7eeaee6084939ecc2e11
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-18 08:56:55 +0530

    :bento: Add palindrome linked list

---
+++ src/Implementations/LinkedLists/palindrome_linked_list.go
@@ -0,0 +1,25 @@
+package LinkedLists
+
+// Problem Statement: Check if the given linked list is a palindrome or no
+
+// Solution: We find the mid point of the linked list. Break the list into
+// two parts. Reverse one of the linked list and compare both of them.
+// We can use Floyd's algorithm to find the mid point of the list.
+func IsPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	slow = ReverseLinkedList2(slow)
+	fast = head
+	for slow != nil {
+		if slow.Val != fast.Val {
+			return false
+		}
+		slow, fast = slow.Next, fast.Next
+	}
+	return true
+}
